8a9e2103d06ae61c206a5df39413b7dd1eb2471e
Matthew DuBois <147000029+duboimd@users.noreply.github.com>
2024-11-05 10:57:11 -0700

    Create string operations.py

---
+++ string operations.py
@@ -0,0 +1,38 @@
+# STRING OPERATIONS:
+print("Question 1:")
+# Q1: Create a string that contains your name called myName. Then, print the 1st and last letter of your name. 
+
+print()
+print("Question 2:")
+# Q2: Using str1 and slicing, print only "coding" from str1
+# Do not touch this code:
+str1 = "I love coding"
+# Write your code below:
+
+print()
+print("Question 3:")
+# Q3: Using str1 and slicing, print only "Hello" from str1
+# Do not touch this code:
+str1 = "Hello world!"
+# Write your code below:
+
+print()
+print("Question 4:")
+# Q4: Slice halloweenString into a new string called newstring using:
+# Start = 3
+# Stop = 20
+# Step = 2
+halloweenString = "Halloween is coming up! BOO!"
+# Write your code below:
+
+print()
+print("Question 5:")
+# Q5: Create a string called favAnimal that contains your favorite animal.
+# a. Using .upper, print favAnimal so it’s all uppercase
+# b. Using .lower, print favAnimal so it’s all lowercase
+# c. Print the length of the string
+#d. Print the first letter and the last letter
+
+print()
+print("Question 6:")
+# Q6: Create a string that contains your favourite color and then print it reversed! 
